07b33342e8aab9407d6233a7f9d8a36174dc16ce
André Primo de Oliveira <andre.webflix@gmail.com>
2025-08-19 14:09:03 -0300

    ajustes em funções e criação da função de busca de quarto em hotel

--- src/Static/Api.php
+++ src/Static/Api.php
@@ -97,6 +97,52 @@ class Api extends \Ezlink\Core\EzlinkStatic {
         }
     }
 
+    private function searchHotel(array $body) {
+        try {
+            $destino = null;
+            $resultHotel = $this->hotels([
+                'skip' => $body['skip'] ?? 0,
+                'limit' => $body['limit'],
+                'order' => 'asc',
+                'countryISO2' => $body['country'],
+            ]);
+            $busca = $body['VariationsHotelsSearched'];
+            [$hotelListResults, $totalAfterFilter] = $resultHotel->hotelListResponse;
+            if ($totalAfterFilter > 0) {
+                foreach ($hotelListResults->hotels as $index => $hotel) {
+                    if (in_array($hotel->name, $busca)) {
+                        $destino = $hotel;
+                        break;
+                    }
+                }
+            }
+            if($destino || $totalAfterFilter == 0) {
+                return $destino;
+            }else {
+                $body['skip'] += $body['limit'];
+                return $this->searchHotel($body);
+            }
+        } catch (\GuzzleHttp\Exception\ServerException $ex) {
+
+            throw \Ezlink\Exceptions\EzlinkException::fromGuzzleException($ex);
+
+        } catch (\GuzzleHttp\Exception\ClientException $ex) {
+
+            throw \Ezlink\Exceptions\EzlinkException::fromGuzzleException($ex);
+
+        } catch (\GuzzleHttp\Exception\BadResponseException $ex) {
+
+            throw \Ezlink\Exceptions\EzlinkException::fromGuzzleException($ex);
+
+        } catch (\GuzzleHttp\Exception\RequestException $ex) {
+
+            throw \Ezlink\Exceptions\EzlinkException::fromGuzzleException($ex);
+
+        } catch (\Exception $ex) {
+            throw new \Ezlink\Exceptions\EzlinkException($ex);
+        }
+    }
+
     private function searchDestination(array $body) {
         try {
             $destino = null;
@@ -143,7 +189,48 @@ class Api extends \Ezlink\Core\EzlinkStatic {
         }
     }
 
-    public function searchHotelInDestination(array $body) {
+    public function searchByHotel(array $body) {
+        try {
+            $destino = $this->searchHotel($body);
+            if($destino) {
+                $apiHotel = new \Ezlink\Hotel\Api();
+                return $apiHotel->searchByDestinationOrHotelId([
+                    'checkIn' => $body['checkIn'],
+                    'checkOut' => $body['checkIn'],
+                    'hotelIds' => [$destino->id],
+                    'nationality' => $body['nationality'],
+                    'timeout' => 15000,
+                    'hotelInfo' => true,
+                    'rooms' => $body['rooms'],
+                ]);
+            }else{
+                return [
+                    "Status" => "Error",
+                    "Code" => ""
+                ];
+            }
+        } catch (\GuzzleHttp\Exception\ServerException $ex) {
+
+            throw \Ezlink\Exceptions\EzlinkException::fromGuzzleException($ex);
+
+        } catch (\GuzzleHttp\Exception\ClientException $ex) {
+
+            throw \Ezlink\Exceptions\EzlinkException::fromGuzzleException($ex);
+
+        } catch (\GuzzleHttp\Exception\BadResponseException $ex) {
+
+            throw \Ezlink\Exceptions\EzlinkException::fromGuzzleException($ex);
+
+        } catch (\GuzzleHttp\Exception\RequestException $ex) {
+
+            throw \Ezlink\Exceptions\EzlinkException::fromGuzzleException($ex);
+
+        } catch (\Exception $ex) {
+            throw new \Ezlink\Exceptions\EzlinkException($ex);
+        }
+    }
+
+    public function searchByHotelInDestination(array $body) {
         try {
             $destino = $this->searchDestination($body);
             if($destino) {
